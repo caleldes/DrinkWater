73d1b906110e159cc02f34174b1cba3d438c2a5e
caleldes <natalia742@gmail.com>
2018-11-24 23:27:43 +0100

    faucet + axios

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button, TouchableOpacity, Image, Dimensions} from 'react-native';
 import { Constants, Location, Permissions } from 'expo';
+import axios from 'axios';
 
 export default class HomeScreen extends React.Component {
-  // state = {
-  //     location: null,
-  //     errorMessage: null,
-  // };
   static navigationOptions = {
     header: null,
   };
@@ -15,85 +12,71 @@ export default class HomeScreen extends React.Component {
       super(props);
 
       this.state = {
-          status: 'Press button...',
+          status: 'Can I drink water here?',
       };
   }
 
-  // componentWillMount() {
-  //     if (Platform.OS === 'android' && !Constants.isDevice) {
-  //         this.setState({
-  //             errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-  //         });
-  //     } else {
-  //         this._getLocationAsync();
-  //     }
-  // }
+  getWaterCondition(cityName){
+      axios.get(`http://192.168.43.213:8080/api/water-conditions/name/${cityName}`).then((result) => {
+          console.log(result.request.response);
+          }
+      ).catch(error => {
+          console.log(error);
+      })
+  }
 
-  // _getLocationAsync = async () => {
-  //     let { status } = await Permissions.askAsync(Permissions.LOCATION);
-  //     if (status !== 'granted') {
-  //         this.setState({
-  //             errorMessage: 'Permission to access location was denied',
-  //         });
-  //     }
-  //     let location = await Location.getCurrentPositionAsync({});
-  //     this.setState({ location });
-  // };
+  async getLocation() {
+      this.setState({
+          status: 'Finding location...',
+      });
 
-    async requestLocation() {
-        this.setState({
-            status: 'Requesting geocode...',
-        });
+      console.log("Ask for location permission...");
+      let {status} = await Permissions.askAsync(Permissions.LOCATION);
+      console.log("Got back: " + status);
 
-        console.log("Ask for location permission...");
-        let {status} = await Permissions.askAsync(Permissions.LOCATION);
-        console.log("Got back: " + status);
+      if (status !== 'granted') {
+          console.log("Failed to get geolocation: access not granted, status " + status);
+          this.setState({
+              status: status,
+          });
+          return;
+      }
+      try {
+          console.log("Request geocode...");
+          let location = await Location.getCurrentPositionAsync({});
+          console.log(location);
+          const {latitude, longitude} = location.coords;
+          let geocode = await Location.reverseGeocodeAsync({latitude, longitude});
+          console.log("Geocode:");
+          console.log(geocode);
 
-        if (status !== 'granted') {
-            console.log("Failed to get geolocation: access not granted, status " + status);
-            this.setState({
-                status: status,
-            });
-            return;
-        }
-        try {
-            console.log("Request geocode...");
-            let location = await Location.getCurrentPositionAsync({});
-            console.log(location);
-            const {latitude, longitude} = location.coords;
-            // console.log(option);
-            let geocode = await Location.reverseGeocodeAsync({latitude, longitude});
-            console.log("Geocode:");
-            console.log(geocode);
+          console.log("DONE");
+          this.setState({
+              status: 'We are in ' + geocode[0].city,
+          });
+          this.getWaterCondition(geocode[0].city);
 
-            console.log("DONE");
-            this.setState({
-                status: 'DONE, we are in ' + geocode[0].city,
-            });
+      }
+      catch (error) {
+          console.log("Failed to get geolocation:");
+          this.setState({
+              status: error,
+          });
+          console.log(error);
+      }
+  }
 
-        }
-        catch (error) {
-            console.log("Failed to get geolocation:");
-            this.setState({
-                status: error,
-            });
-            console.log(error);
-        }
-    }
-
-    render() {
-        // let text = 'Waiting..';
-        // if (this.state.errorMessage) {
-        //     text = this.state.errorMessage;
-        // } else if (this.state.location) {
-        //     text = JSON.stringify(this.state.location);
-        // }
-    return (
-        <View style={styles.container}>
-          {/*<Text style={styles.paragraph}>{text}</Text>*/}
-          <Button title="Request Location" onPress={() => this.requestLocation()} />
-          <Text>{this.state.status}</Text>
-        </View>
+  render() {
+  return (
+      <View style={styles.container}>
+          <Text style={styles.status}>{this.state.status}</Text>
+          <TouchableOpacity onPress={() => this.getLocation()} style={styles.button}>
+                <Image
+                    style={styles.image}
+                     source={require('./faucet.jpg')}
+                />
+          </TouchableOpacity>
+      </View>
     );
   }
 }
@@ -104,11 +87,31 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         paddingTop: Constants.statusBarHeight,
-        backgroundColor: '#91e8ff',
+        backgroundColor: 'steelblue',
     },
     paragraph: {
         margin: 24,
         fontSize: 18,
         textAlign: 'center',
     },
+    image: {
+
+        width: Dimensions.get('window').width - 180,
+        height: Dimensions.get('window').width - 180,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    status: {
+        marginBottom: 80,
+        color: 'skyblue',
+        fontSize: 30,
+    },
+    button: {
+        width: Dimensions.get('window').width - 100,
+        height: Dimensions.get('window').width - 100,
+        backgroundColor: 'skyblue',
+        borderRadius: 160,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
